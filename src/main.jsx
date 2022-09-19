47f78f79ce62412b2ffadbda986030012b6ccb1b
Jesus Herrera <heisusin@gmail.com>
2022-09-20 01:01:22 +0200

    perf: 🎨 Added "NextUIProvider" to App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { NextUIProvider } from '@nextui-org/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
-)
+	<NextUIProvider>
+		<App />
+	</NextUIProvider>
+);
