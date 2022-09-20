5cc35ec5c05acc086fb38e9b2b3a739049e712c1
Jesus Herrera <heisusin@gmail.com>
2022-09-20 02:01:33 +0200

    style: 💄 Added styles to Sidebar component

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,17 +1,22 @@
+import { Container } from '@nextui-org/react'
 import React from 'react'
+import './Sidebar.css'
+
 
 const Sidebar = () => {
   return (
-    <main>
-      <article>
-        <button><i className='bx bx-task' ></i> Task</button>
-        <button><i className='bx bx-book-add' ></i></button>
-      </article>
-      <article>
-        <button><i className='bx bxs-user'></i> Employees</button>
-        <button><i className='bx bxs-user-plus' ></i></button>
-      </article>
-    </main>
+    <Container className='container__sidebar'>
+      <section className="sidebar__btn--section">
+        <article className='sidebar__btn--article'>
+          <button className='sidebar__task--btn'><i className='bx bx-task' ></i> Task</button>
+          <button className='sidebar__icon--btn'><i className='add__icon--btn bx bx-book-add' ></i></button>
+        </article>
+        <article className='sidebar__btn--article'>
+          <button className='sidebar__employee--btn'><i className=' bx bxs-user'></i> Employees</button>
+          <button className='sidebar__icon--btn'><i className='add__icon--btn bx bxs-user-plus' ></i></button>
+        </article>
+      </section>
+    </Container>
   )
 }
 
