210b63f8b84b08b26d62c8da958793307b3d0830
Jesus Herrera <heisusin@gmail.com>
2022-09-20 02:03:36 +0200

    style: 💄 Applied Grid from NextUI to centered the dashboard components

--- src/Pages/DashboardAdmin/DashboardAdmin.jsx
+++ src/Pages/DashboardAdmin/DashboardAdmin.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Navbar from '../../Components/TopMenu/TopMenu'
-import Sidebar from '../../Components/Sidebar/Sidebar'
 
 const DashboardAdmin = () => {
   return (
     <>
-      <Navbar />
-      <Sidebar />
+      
     </>
   )
 }
--- src/Pages/DashboardUser/DashboardUser.jsx
+++ src/Pages/DashboardUser/DashboardUser.jsx
@@ -1,12 +1,21 @@
+import { Container, Grid } from '@nextui-org/react'
 import React from 'react'
-import Navbar from '../../Components/TopMenu/TopMenu'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import './DashboardUser.css'
 
 const DashboardUser = () => {
   return (
     <>
-      <Navbar />
-      <Sidebar />
+    <main className="container__dashboard">
+      <Grid.Container gap={2} justify='center'>
+        <Grid md={2}>
+          <Sidebar />
+        </Grid>
+        <Grid md={10}>
+          <Container>hola</Container>
+        </Grid>
+      </Grid.Container>
+    </main>
     </>
   )
 }
