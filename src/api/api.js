056c0e702799e3fac8ab56e2da052665d4508e41
Sefi Cohen <67965039+seficohen1@users.noreply.github.com>
2022-09-25 18:46:41 +0200

    moved all api calls to api file

--- src/api/api.js
+++ src/api/api.js
@@ -1,41 +1,74 @@
 import { set } from "react-hook-form";
+import axios from "axios";
+import Swal from "sweetalert2";
 
-
-
-
-
-async function fetchTasks (setState) {
+async function fetchTasks(setState) {
   try {
-    const res = await fetch('http://localhost:4001/dashboard/tasks')
+    const res = await fetch("http://localhost:4001/dashboard/tasks");
     const data = await res.json();
     const results = data.results;
     setState(results);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
-
-async function updateTask (dbId, body) {
-  console.log(dbId.length)
-  console.log(JSON.stringify(body))
+async function updateTask(dbId, body) {
+  console.log(dbId.length);
+  console.log(JSON.stringify(body));
   const options = {
-    method: 'PATCH',
-    mode: 'cors',
+    method: "PATCH",
+    mode: "cors",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(body)
-  }
+    body: JSON.stringify(body),
+  };
   try {
-    const baseUrl = 'http://localhost:4001/dashboard/tasks/';
-    const res = await fetch(`${baseUrl}${dbId}`, options)
+    const baseUrl = "http://localhost:4001/dashboard/tasks/";
+    const res = await fetch(`${baseUrl}${dbId}`, options);
     const data = await res.json();
-    console.log(data)
+    console.log(data);
   } catch (error) {
-    console.log(error)
-    
+    console.log(error);
   }
 }
 
-export { fetchTasks, updateTask }
+// edit user  details
+
+const updateUser = async (url, options) => {
+  const { firstName, lastName, email, role, dropDownInput } = options;
+
+  try {
+    const request = await axios.patch(url, {
+      firstName,
+      lastName,
+      email,
+      role: role === "undefined" ? "user" : dropDownInput,
+    });
+    if (request.status === 200) {
+      Swal.fire({
+        icon: "success",
+        text: "User was saved successfully",
+      });
+    } else {
+      Swal.fire({
+        icon: "warning",
+        text: "Oops, something went wrong!",
+      });
+    }
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+const getAllUsers = async (url, setState) => {
+  try {
+    const request = await axios(url);
+    if (request.status === 200) setState(request.data.results);
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+export { fetchTasks, updateTask, updateUser, getAllUsers };
