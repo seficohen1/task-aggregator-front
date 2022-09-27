fa8f39c5d66dc3f359e6d603ca0db2231b10aa32
Jesus Herrera <heisusin@gmail.com>
2022-09-27 13:46:54 +0200

    Created render button if is admin or user

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,24 @@
 import { Container, Grid, Button } from "@nextui-org/react";
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import TaskContainer from "../../Components/TaskContainer/TaskContainer";
 import "./Dashboard.css";
 import TopMenu from "../../Components/TopMenu/TopMenu";
 import UserDetail from "../UserDetail/UserDetail";
 import { useNavigate } from 'react-router-dom';
+import AuthContext from '../../context/AuthContext';
+
 
 
 const DashboardUser = () => {
+  const { user } = useContext(AuthContext)
   const [isTaskSelected, setIsTaskSelected] = useState(true);
   const navigate = useNavigate();
+  console.log(user.role)
+  
+  const isAdmin = user.role === 'admin' ? true : false
+  
+ 
 
   return (
     <>
@@ -45,7 +53,8 @@ const DashboardUser = () => {
                     Employees
                   </Button>
                 </article>
-              <Button className='taskmenu__office--btn' size='xs'onClick={() => {navigate("/admin")}}>OFFICE</Button>
+                {isAdmin ? <Button className="taskmenu__office--btn" size='xs' onClick={() => {navigate("/admin")}}>OFFICE</Button> : ''}
+              {/* <Button className='taskmenu__office--btn' size='xs'onClick={() => {navigate("/admin")}}>OFFICE</Button> */}
               </Container>
               {isTaskSelected ? <TaskContainer /> : <UserDetail />}
               {/* <TaskContainer /> */}
